d18df547592d133d2ca1f03f7f2a8ca4e2a07339
yousseffehmi96 <yousseffehmi96@icloud.com>
2026-03-11 12:01:32 +0100

    update

--- main.py
+++ main.py
@@ -10,18 +10,12 @@ Base.metadata.create_all(bind=engine)
 app.include_router(api_router)
 app.include_router(societe_router)
 app.include_router(Leads_router)
-origins = [
-    "http://localhost:3000",
-    "https://front-lead-clean.vercel.app/"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["https://front-lead-clean.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
